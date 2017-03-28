6d89b9aed2dae5749d68824bc04d9861280a3ce4
elix-x <Elix-x@users.noreply.github.com>
2017-03-28 22:10:45 +0200

    Release deobf only to gtr - try 5

--- githubreleases.sh
+++ githubreleases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export GITHUB_RELEASES_FILES="[$(ls build/libs/*.jar | grep -v obf | tr '\n' ' ' | sed -e 's/[[:space:]]*$//' | tr ' ' ',')]"
+export GITHUB_RELEASES_FILES="[$(ls build/libs/*.jar | grep -v obf | tr '\n' ' ' | sed -e 's/[[:space:]]*$//' | sed 's/\s/, /g')]"
 echo $GITHUB_RELEASES_FILES
